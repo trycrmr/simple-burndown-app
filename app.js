39ccb38ee296c504e607b12e56920dff9c135538
Terry Creamer <trycrmr@gmail.com>
2017-11-19 08:20:03 -0500

    Validates ejs works as expected

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let express = require('express')
 let app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  var ejs = require('ejs'),
+    people = ['geddy', 'neil', 'alex'],
+    html = ejs.render('<%= people.join(", "); %>', {people: people});
+  res.send(html)
+})
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Simple Burndown App listening on port 3000!'))
