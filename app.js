fb542ec1231cdf080d44e5681487336c0e5cbbdc
Terry Creamer <trycrmr@gmail.com>
2017-11-24 12:29:54 -0500

    Adds /data endpoint

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let express = require('express')
 let app = express()
 let path = require('path');
 let ejs = require('ejs');
+let db = require('./config/db')
+let async = require('asyncawait/async');
+let await = require('asyncawait/await');
 
 let viewPath = path.join(__dirname, '/views');
 // app.set('views', viewPath);
@@ -10,14 +13,24 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.render('home', {foo: 'foo'});
+  res.render('home');
 })
 
-app.get('/data', (req, res) => {
-  res.send([
-    {id: 'terry'},
-    {id: 'justin'}
-  ]);
-})
+app.get('/data', async( (req, res) => {
+  try {
+    let results = await(db.query(
+`
+SELECT date, persons.name AS person, projects.name AS project, time, type FROM time_entry_fac
+INNER JOIN persons ON time_entry_fac.person = persons.id
+INNER JOIN projects ON time_entry_fac.project = projects.id
+`
+    ))
+    console.log('results.rows', results.rows);
+    res.send(results.rows);
+  } catch(err) {
+    console.log('Error getting data from db', err);
+    res.status(500).json(err)
+  }
+}))
 
 app.listen(3000, () => console.log('Simple Burndown App listening on port 3000!'))
